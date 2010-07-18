934031aab618e9103a472f15eb4effd96d779968
david.villa <none@none>
2010-07-18 21:48:56 +0000

    new release
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405012

--- tex/parts.sh
+++ tex/parts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MAIN=${1:-main.tex}
 echo $MAIN
-grep input $MAIN | grep -v "^%" | awk -F{ '{print $2}' | awk -F} '{print $1}'
+grep "\\input{" $MAIN | grep -v "^%" | awk -F{ '{print $2}' | awk -F} '{print $1}'
