d8247bc7b87ee5db0ff67b5659a1bc91ffa6db46
david.villa <none@none>
2010-09-14 20:08:11 +0000

    adaptación a la nueva sintaxis de pdfnup
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405096

--- make/slide.mk
+++ make/slide.mk
@@ -1,30 +1,30 @@
 # requires: unovonv, pdfjam, pdftk
-#
-# SRC=$(wildcard *.odp)
-# PDF1=$(notdir $(patsubst %.odp, %.pdf, $(SRC)))
-# PDF3=$(patsubst %.pdf, %.1x3.pdf, $(PDF1))
-# PDFM=$(patsubst %.pdf, %.mor.pdf, $(PDF1))
-#
-# PDF=$(PDF1) $(PDF3) $(PDFM)
-#
-# all:  $(PDF)
+
+SRC=$(wildcard *.odp)
+PDF1=$(notdir $(patsubst %.odp, %.pdf, $(SRC)))
+PDF3=$(patsubst %.pdf, %.1x3.pdf, $(PDF1))
+PDFM=$(patsubst %.pdf, %.mor.pdf, $(PDF1))
+
+PDF=$(PDF1) $(PDF3) $(PDFM)
+
+all:  $(PDF)
 
 %.pdf: %.odp
 	odt2pdf $<
 
 %.1x3.pdf: %.pdf
-	pdfnup --nup 1x3 --offset "-3cm 0" --trim "-2cm -2cm -2cm -2cm" $< --outfile $@
+	pdfnup --no-landscape --nup 1x3 --offset "-3cm 0" --trim "-2cm -2cm -2cm -2cm" $< --outfile $@
 
 %.first.pdf: %.pdf
-	pdfnup --pages 1 --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 1x1 $< --outfile $@
+	pdfnup --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 1x1 $< 1 --outfile $@
 
 %.2x2.pdf: %.pdf
-	pdfnup --pages 2- --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 2x2 $< --outfile $@
+	pdfnup --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 2x2 $< 2- --outfile $@
 
 %.mor.pdf: %.first.pdf %.2x2.pdf
 	pdftk $^ cat output aux-$@
-	pdfnup --nup 1x2 --paper a4paper --scale 0.9 --delta "0.45cm 0.45cm" aux-$@ --outfile $@
+	pdfnup --no-landscape --nup 1x2 --paper a4paper --scale 0.9 --delta "0.45cm 0.45cm" aux-$@ --outfile $@
 	$(RM) aux-$@
 
-# clean:
-# 	$(RM) temp *~ $(PDF)
+clean:
+	$(RM) temp *~ $(PDF)
