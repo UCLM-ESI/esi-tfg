ec9d6c2c8f4752f398293e6675d1c8e10120a612
david.villa <none@none>
2010-09-14 21:16:50 +0000

    --HG-- extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405097

--- make/slide.mk
+++ make/slide.mk
@@ -3,7 +3,7 @@
 SRC=$(wildcard *.odp)
 PDF1=$(notdir $(patsubst %.odp, %.pdf, $(SRC)))
 PDF3=$(patsubst %.pdf, %.1x3.pdf, $(PDF1))
-PDFM=$(patsubst %.pdf, %.mor.pdf, $(PDF1))
+PDFM=$(patsubst %.pdf, %.2x4.pdf, $(PDF1))
 
 PDF=$(PDF1) $(PDF3) $(PDFM)
 
@@ -13,7 +13,7 @@ all:  $(PDF)
 	odt2pdf $<
 
 %.1x3.pdf: %.pdf
-	pdfnup --no-landscape --nup 1x3 --offset "-3cm 0" --trim "-2cm -2cm -2cm -2cm" $< --outfile $@
+	pdfnup --no-landscape --paper a4paper --scale 0.9 --delta "0.8cm 0.8cm" --frame true --offset "-3cm 0" --nup 1x3 $< --outfile $@
 
 %.first.pdf: %.pdf
 	pdfnup --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 1x1 $< 1 --outfile $@
@@ -21,7 +21,7 @@ all:  $(PDF)
 %.2x2.pdf: %.pdf
 	pdfnup --paper a4paper --delta "0.8cm 0.8cm" --frame true --nup 2x2 $< 2- --outfile $@
 
-%.mor.pdf: %.first.pdf %.2x2.pdf
+%.2x4.pdf: %.first.pdf %.2x2.pdf
 	pdftk $^ cat output aux-$@
 	pdfnup --no-landscape --nup 1x2 --paper a4paper --scale 0.9 --delta "0.45cm 0.45cm" aux-$@ --outfile $@
 	$(RM) aux-$@
