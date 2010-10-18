afb8897bffee18ce6d3b9df7f24846ed5db40d89
david.villa <none@none>
2010-10-18 22:14:01 +0000

    --HG-- extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405158

---
+++ emacs/config/speedbar.cfg.el
@@ -0,0 +1,6 @@
+(custom-set-variables
+ '(speedbar-supported-extension-expressions (quote (".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?" ".tex\\(i\\(nfo\\)?\\)?" ".el" ".emacs" ".l" ".lsp" ".p" ".java" ".js" ".f\\(90\\|77\\|or\\)?" ".ada" ".p[lm]" ".tcl" ".m" ".scm" ".pm" ".py" ".g" ".s?html" ".ma?k" "[Mm]akefile\\(\\.in\\)?" ".ice"))))
+
+(global-set-key [f9] 'speedbar)
+
+(setq speedbar-default-position (quote right))
