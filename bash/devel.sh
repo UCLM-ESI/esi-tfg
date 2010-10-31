41cfb25c695ca5cb9c54b6018cc948546f5acdff
david.villa <none@none>
2010-10-31 11:13:03 +0000

    fmake -> imake, ahora con gui-notifications
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405191

--- bash/devel.sh
+++ bash/devel.sh
@@ -3,4 +3,27 @@
 # To use that functions and aliases, put next line in your .bashrc:
 #   source /usr/share/arco-tools/devel.sh
 
-alias fmake='while [ 1 ]; do make; echo DONE:$?; inotifywait -qr -e MODIFY . || break; done;'
+function iexec() {
+	cmd=$*
+
+	while [ 1 ]; do
+		$cmd
+		retval=$?
+
+		echo DONE:$retval
+
+		if type notify-send > /dev/null; then
+			if [ $retval -eq 0 ]; then
+				icon='gtk-yes'
+			else
+				icon='gtk-no'
+			fi
+			notify-send -t 2000 -i $icon "fmake: $retval" "$ $cmd"
+		fi
+		inotifywait -qr -e MODIFY . || break
+		sync
+		sleep 0.5
+	done
+}
+
+alias imake='iexec make'
