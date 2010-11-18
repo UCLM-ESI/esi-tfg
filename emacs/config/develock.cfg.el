4134cd046a203e06197e13fd1f9d03bf8b68c9f1
david.villa <none@none>
2010-11-18 22:15:45 +0000

    config "razonable" para develock
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405285

---
+++ emacs/config/develock.cfg.el
@@ -0,0 +1,5 @@
+(custom-set-faces
+ '(develock-long-line-2 ((t (:background "gray90" :foreground "red"))))
+ '(develock-whitespace-1 ((t (:background "light gray"))))
+ '(develock-whitespace-2 ((t (:background "gray"))))
+ '(develock-whitespace-3 ((t (:background "gray90")))))
