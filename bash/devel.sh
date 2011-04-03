4b87e58c970f52d5fce4bdeb7e338085e97f8352
david.villa <none@none>
2011-04-03 09:52:21 +0000

    replacing fexec by aexec
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405545

--- bash/devel.sh
+++ bash/devel.sh
@@ -4,32 +4,35 @@
 # To use that functions and aliases, put next line in your .bashrc:
 #   source /usr/share/arco-tools/devel.sh
 
-function fexec() {
-	cmd=$*
+# function fexec() {
+# 	cmd=$*
+#
+# 	while [ 1 ]; do
+# 		if type notify-send > /dev/null; then
+# 			notify-send -t 1500 -i gtk-info "fexec: starting" "$cmd" &
+# 		fi
+#
+# 		$cmd
+# 		retval=$?
+#
+# 		echo DONE:$retval
+#
+# 		if type notify-send > /dev/null; then
+# 			if [ $retval -eq 0 ]; then
+# 				icon='gtk-yes'
+# 			else
+# 				icon='gtk-no'
+# 			fi
+# 			notify-send -t 2000 -i $icon "fexec: $retval" "$ $cmd"
+# 		fi
+# 		inotifywait -qr -e MODIFY . @*~ @.* @#*# || break
+# 		clear
+# 		sync
+# 		sleep 0.5
+# 	done
+# }
 
-	while [ 1 ]; do
-		if type notify-send > /dev/null; then
-			notify-send -t 1500 -i gtk-info "fexec: starting" "$cmd" &
-		fi
+alias fexec='fexec is deprecated, use aexec'
+alias fmake='fmake is deprecated, use amake'
 
-		$cmd
-		retval=$?
-
-		echo DONE:$retval
-
-		if type notify-send > /dev/null; then
-			if [ $retval -eq 0 ]; then
-				icon='gtk-yes'
-			else
-				icon='gtk-no'
-			fi
-			notify-send -t 2000 -i $icon "fexec: $retval" "$ $cmd"
-		fi
-		inotifywait -qr -e MODIFY . @*~ @.* @#*# || break
-		clear
-		sync
-		sleep 0.5
-	done
-}
-
-alias fmake='fexec make'
+alias amake='aexec make'
