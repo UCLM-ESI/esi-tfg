86dcf10802363b17c140ca4631a102d905067e8c
david.villa <none@none>
2011-06-16 22:45:04 +0000

    - inclusión de partes recursivas (\input dentro de \input) - ajustes en las distancias de las portadas según normativa ESI
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405804

--- tex/parts-tex.sh
+++ tex/parts-tex.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+function parts() {
+    for f in $(grep "\\input{" $1 | grep -v "^%" | awk -F{ '{print $2}' | awk -F} '{print $1}'); do
+         echo $f
+	 parts $f
+    done
+}
+
+
 MAIN=${1:-main.tex}
 echo $MAIN
-grep "\\input{" $MAIN | grep -v "^%" | awk -F{ '{print $2}' | awk -F} '{print $1}'
+parts $MAIN
