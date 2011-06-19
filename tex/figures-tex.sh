4463a9fa1dd6765a81782087310f9a98517de148
david.villa <none@none>
2011-06-19 21:16:25 +0000

    Error "sed: -e expression #1, char 51: Invalid range end" fixed
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405831

--- tex/figures-tex.sh
+++ tex/figures-tex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MAIN=${1:-main.tex}
-[ -f $MAIN ] && grep -v "^%" $MAIN | grep includegraphics \
-    | sed "s/.*includegraphics.*{\([.a-zA-Z0-9_-//]*\).*$/\1/g" \
+[ -f $MAIN ] && grep -v "^%" $MAIN \
+    | grep includegraphics \
+    | sed "s/.*includegraphics.*{\([.a-zA-Z0-9_//\-]*\).*$/\1/g" \
     | sort | uniq
+    
