431b8184803eccc24aba0e3343ab2112cd991c98
david.villa <none@none>
2011-07-07 15:37:35 +0000

    new release
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405866

--- emacs/config/auto-insert.cfg.el
+++ emacs/config/auto-insert.cfg.el
@@ -6,11 +6,10 @@
 (setq auto-insert-directory "/usr/share/arco-tools/emacs/template/")  ; where the templates are
 (setq auto-insert-query nil)  ; don't ask about insertions
 
-(define-auto-insert "\.py" "header.py")
-(define-auto-insert "\.rb" "header.rb")
-(define-auto-insert "\.sh" "header.sh")
-(define-auto-insert "\.cpp" "header.cpp")
-(define-auto-insert "\.cc" "header.cpp")
-(define-auto-insert "\.tex" "header.tex")
+(define-auto-insert "\.py" "empty.py.tmpl")
+(define-auto-insert "\.rb" "empty.rb.tmpl")
+(define-auto-insert "\.sh" "empty.sh.tmpl")
+(define-auto-insert "\.cpp" "empty.cpp.tmpl")
+(define-auto-insert "\.cc" "empty.cpp.tmpl")
+(define-auto-insert "\.tex" "empty.tex.tmpl")
 (define-auto-insert "main.tex" "main.tex.tmpl")
-
