7b2ac21b08388d43fa67a2a50535c59f55f1a438
david.villa <none@none>
2011-09-24 15:36:24 +0000

    tests
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405904

--- test/make.py
+++ test/make.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8; tab-width:4; mode:python -*-
 
 def make_and_clean(desc, dirname):
-    Test('make clean all', desc=desc, cwd=dirname)
-    Test('make clean', desc=desc, cwd=dirname)
+    Test('make -I $basedir/make clean all', desc=desc, cwd=dirname)
+    Test('make -I $basedir/make clean', desc=desc, cwd=dirname)
