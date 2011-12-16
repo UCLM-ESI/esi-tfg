f84f5bca0325e4d929134f787e12f72e8957d188
david.villa <none@none>
2011-12-16 16:04:42 +0000

    recursive vclean rule
    --HG--
    extra : convert_revision : svn%3A695031e5-78d0-0310-ab2c-c559624c6e18/prj/arco-tools%405961

--- make/subdirs.mk
+++ make/subdirs.mk
@@ -1,11 +1,19 @@
 
-SUBDIRS ?= $(filter-out $(wildcard .*),\
-	$(foreach d,$(shell find . -maxdepth 1 -type d ),$(notdir $(d))))
+.DEFAULT_GOAL=all
+
+# SUBDIRS ?= $(filter-out $(wildcard .*),\
+# 	$(foreach d,$(shell find . -maxdepth 1 -type d ),$(notdir $(d))))
+
+SUBDIR_MAX_DEPTH=5
+SUBDIRS ?= $(sort $(dir $(shell find . -mindepth 2 -maxdepth $(SUBDIR_MAX_DEPTH) -name Makefile)))
+
 
 all:       RULE = all
 install:   RULE = install
 uninstall: RULE = uninstall
 clean:     RULE = clean
+vclean:    RULE = vclean
+
 
 all clean install uninstall:: subdirs
 
